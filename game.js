6e8e115a14807d641f9b66adf4ad25c343664a0f
decktech <mdecker113@gmail.com>
2022-09-16 18:24:08 -0400

    game loop working

--- game.js
+++ game.js
@@ -1,3 +1,22 @@
+let lastRenderTime = 0;
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from './snake.js'
+
 function main(currentTime) {
-    window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  window.requestAnimationFrame(main);
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+  console.log('Render');
+  lastRenderTime = currentTime;
+
+	update()
+	draw()
+}
+// window.requestAnimationFrame(main)
+function update() {
+	updateSnake()
+}
+
+function draw() {
+	drawSnake()
 }
---
+++ snake.js
@@ -0,0 +1,15 @@
+export const SNAKE_SPEED = 1;
+const snakeBody = [{ x: 11, y: 11}];
+
+export function update() {
+  console.log('update')
+}
+
+export function draw(gameBoard) {
+	snakeBody.forEach(segment => {
+		const snakeElement = document.createElement('div');
+		snakeElement.style.gridRowStart
+	})
+}
+
+
