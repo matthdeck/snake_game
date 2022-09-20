f92316c22ed65dce30e1989712518f063898356a
decktech <mdecker113@gmail.com>
2022-09-20 15:24:08 -0400

    game works

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from './snake.js';
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
+import{ outsideGrid } from './grid.js'
 
 let lastRenderTime = 0;
 let gameOver = false;
@@ -7,7 +8,10 @@ let gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
   if (gameOver) {
-    return alert('you lose')
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/';
+    }
+    return;
   }
 
   window.requestAnimationFrame(main);
