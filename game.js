1fc5c7bf801b4b8b78f7a191ef293d45227e5e7f
decktech <mdecker113@gmail.com>
2022-09-20 15:01:24 -0400

    grid

--- game.js
+++ game.js
@@ -1,25 +1,40 @@
 import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from './snake.js';
+import { update as updateFood, draw as drawFood } from './food.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 let gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-  window.requestAnimationFrame(main);
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+  if (gameOver) {
+    return alert('you lose')
+  }
 
-  console.log('Render');
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  // console.log('Render');
   lastRenderTime = currentTime;
 
-	update()
-	draw()
+	update();
+	draw();
 }
-// window.requestAnimationFrame(main)
+
+window.requestAnimationFrame(main);
+
 function update() {
-	updateSnake()
+	updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
-  gameBoard.innerHTML = ''
-	drawSnake(gameBoard)
+  gameBoard.innerHTML = '';
+	drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
